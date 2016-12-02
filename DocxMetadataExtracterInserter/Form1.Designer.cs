ddf4e4ff769e356dce2b1a82989a7625a1a267f9
Amartya Das Sharma <amartyads@hotmail.co.uk>
2016-12-02 15:28:51 +0400

    Add project files.

---
+++ DocxMetadataExtracterInserter/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DocxMetadataExtracterInserter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // chooseFolder
+            // 
+            this.chooseFolder.Location = new System.Drawing.Point(12, 12);
+            this.chooseFolder.Name = "chooseFolder";
+            this.chooseFolder.Size = new System.Drawing.Size(289, 23);
+            this.chooseFolder.TabIndex = 0;
+            this.chooseFolder.Text = "Choose Folder";
+            this.chooseFolder.UseVisualStyleBackColor = true;
+            this.chooseFolder.Click += new System.EventHandler(this.chooseFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 252);
+            this.Controls.Add(this.chooseFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chooseFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ DocxMetadataExtracterInserter/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DocxMetadataExtracterInserter
+{
+    public partial class Form1 : Form
+    {
+        static IMongoClient _client;
+        static IMongoDatabase _database;
+        public Form1()
+        {
+            InitializeComponent();
+            _client = new MongoClient();
+            _database = _client.GetDatabase("CBIRtest");
+        }
+
+        private void chooseFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult res = folderBrowserDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                string dir = folderBrowserDialog1.SelectedPath;
+                extractAndInsertData(dir);
+            }
+        }
+
+        private void extractAndInsertData(string dir)
+        {
+            var collect = _database.GetCollection<BsonDocument>("docxfiles");
+            try
+            {
+                string[] files = Directory.GetFiles(dir, "*.docx");
+                foreach (var fle in files)
+                {
+                    //open file
+                    using (WordprocessingDocument curDoc = WordprocessingDocument.Open(fle, false))
+                    {
+                        string title;
+                        try { title = curDoc.PackageProperties.Title.ToLower(); }
+                        catch { title = ""; }
+
+                        string author;
+                        try { author = curDoc.PackageProperties.Creator.ToLower(); }
+                        catch { author = ""; }
+
+                        string subject;
+                        try { subject = curDoc.PackageProperties.Subject.ToLower(); }
+                        catch { subject = ""; }
+
+                        string category;
+                        try { category = curDoc.PackageProperties.Category.ToLower(); }
+                        catch { category = ""; }
+
+                        string keywords;
+                        try { keywords = curDoc.PackageProperties.Keywords.ToLower(); }
+                        catch { keywords = ""; }
+
+                        string description;
+                        try { description = curDoc.PackageProperties.Description.ToLower(); }
+                        catch { description = ""; }
+
+                        DateTime? created;
+                        try { created = curDoc.PackageProperties.Created; }
+                        catch { created = null; }
+
+                        DateTime? modified;
+                        try { modified = curDoc.PackageProperties.Modified; }
+                        catch { modified = null; }
+
+                        var document = new BsonDocument
+                        {
+                            {"filename", Path.GetFileNameWithoutExtension(fle)},
+                            {"filepath", fle},
+                            {"filetype", "docx" },
+                            {"title", title},
+                            {"author", author},
+                            {"subject", subject},
+                            {"category", category},
+                            {"keywords", keywords},
+                            {"description", description},
+                            {"created", created},
+                            {"modified", modified}
+                        };
+                        collect.InsertOne(document);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("exception");
+            }
+            MessageBox.Show("Done updating database");
+        }
+    }
+}
